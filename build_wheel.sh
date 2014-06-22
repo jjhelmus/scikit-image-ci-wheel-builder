3eb1906bb5b707a19bbac89af0a4bb92c160078e
Jonathan Helmus <jjhelmus@gmail.com>
2014-06-22 07:53:50 -0500

    Testing new build_wheel.sh script

--- build_wheel.sh
+++ build_wheel.sh
@@ -4,15 +4,12 @@ PYTHON_VERSION="2.7"
 NUMPY_VERSION="1.6"         # 1.6 works for 2.7, 1.7 works for 3.3, 1.8 for 3.4
 SKIMAGE_VERSION="0.10.0"        # version of scikits-image to build wheel for
 
-set -x
 # Install miniconda 
 # comment this block out for local builds.
 wget http://repo.continuum.io/miniconda/Miniconda-3.5.2-MacOSX-x86_64.sh \
     -O miniconda.sh
 chmod +x miniconda.sh
 ./miniconda.sh -b
-ls -la
-ls -la ~
 export PATH=/Users/travis/miniconda/bin:$PATH
 conda update --yes conda
 
@@ -22,14 +19,13 @@ source activate build_env
 conda install --yes setuptools pip numpy=$NUMPY_VERSION six cython
 python --version
 
-# Download the the scikits-image release files
+# Download the scikits-image release files
 mkdir tmp
 cd tmp
-
 #cp ../v${SKIMAGE_VERSION}.tar.gz .  # testing locally
 wget --no-check-certificate \
-    https://pypi.python.org/packages/source/s/scikit-image/scikit-image-${VERSION}.tar.gz \
-    -O v${VERSION}.tar.gz
+    https://pypi.python.org/packages/source/s/scikit-image/scikit-image-${SKIMAGE_VERSION}.tar.gz \
+    -O v${SKIMAGE_VERSION}.tar.gz
 tar xfz v${SKIMAGE_VERSION}.tar.gz
 cd scikit-image-${SKIMAGE_VERSION}/
 
@@ -38,9 +34,8 @@ echo "Building scikits-image wheel for Python: $PYTHON_VERSION"
 pip install wheel
 python setup.py bdist_wheel
 
-mv dist/*.whl ../../.
-
 # clean up
+mv dist/*.whl ../../.
 cd ../..
 rm -r tmp
 source deactivate
